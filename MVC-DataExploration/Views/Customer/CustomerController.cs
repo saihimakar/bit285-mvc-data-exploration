87bf36846be6b2ca413ee05043a6028ddc36a2db
Sri <Sri@LENOVO-PC>
2017-01-23 23:58:50 -0800

    Still working

---
+++ MVC-DataExploration/Views/Customer/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_DataExploration.Models;
+namespace MVC_DataExploration.Views.Customer
+{
+    public class CustomerController : Controller
+    {
+        private MyDbContext db = new MyDbContext();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+            //return View(db.Customers);
+        }
+
+        [HttpGet]
+        public ActionResult Create ()
+        {
+            return View(); // show the form ( the little boxes)
+        }
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            // Add the new customer to my set of Customers 
+            db.Customers.Add(customer);
+            // Updates Changes
+            db.SaveChanges();
+            
+            
+            return View("Index", db.Customers); //show the updated Index page
+        }
+    }
+}
